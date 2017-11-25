8ebbd4ba654ffef3bd7e25c89fec9cdaa80dcfc2
hebaragiseed <hebaragiseed@gmail.com>
2017-11-25 14:44:33 +0900

    Redux middleware logger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from 'redux/configureStore';
+import 'index.css';
+import App from 'App';
 
+store.dispatch({type: 'BULLSHIT'});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
+
 
