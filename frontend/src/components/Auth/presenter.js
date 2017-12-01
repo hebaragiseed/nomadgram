0fb3e7ba52c3ffef176d06811c15eec5dbb1ae9e
hebaragiseed <hebaragiseed@gmail.com>
2017-12-02 00:53:15 +0900

    Auth component part two

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,4 +1,40 @@
 import React from 'react';
+import styles from './styles.scss';
 
-const Auth = (props, context) => <h1>what up</h1>;
+const Auth = (props, context) => (
+  <main className={styles.auth}>
+    <div className={styles.column}>
+      <img className={styles.phone} src={require('images/phone.png')} alt="Chekout our app. Is cool" />
+    </div>
+    <div className={styles.column}>
+      <div className={styles.whiteBox}>
+        {(() => {
+          switch (props.action) {
+            case 'login':
+              return (
+                <p>Don't have an account?{" "}
+                  <span onClick={props.changeAction} className={styles.changeLink}>Sign up</span>
+                </p>
+              );
+              case 'signup':
+                return (
+                  <p>Have an account{' '}
+                  <span onClick={props.changeAction} className={styles.changeLink}>Login</span>
+                  </p>
+                );
+            default:
+              null;
+          }
+        })()}
+      </div>
+      <div className={styles.appBox}>
+        <span>Get the app</span>
+        <div className={styles.appstores}>
+          <img src={require('images/ios.png')} alt="Download it on the Apple Appstore" />
+          <img src={require('images/android.png')} alt="Download it on the Android Googleplay" />
+        </div>
+      </div>
+    </div>
+  </main>
+);
 export default Auth;
